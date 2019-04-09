3ae45eb0f1c4897500da04489e02eac39f8ff4a6
iut <iut@10.0.2.15>
2019-04-09 15:09:37 +0200

    ajout position vaisseau + positions impossibles

---
+++ src/main/java/fr/unilim/fr/spaceinvaders/utils/HorsEspaceJeuException.java
@@ -0,0 +1,10 @@
+package fr.unilim.fr.spaceinvaders.utils;
+
+@SuppressWarnings("serial")
+public class HorsEspaceJeuException extends RuntimeException {
+
+    public HorsEspaceJeuException(String message) {
+	    super(message);
+    }
+
+}
