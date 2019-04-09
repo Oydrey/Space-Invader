2556f281c18f19de18f3f7a49572e98998724944
iut <iut@10.0.2.15>
2019-04-09 15:22:27 +0200

    ajout déplacement vaisseau

--- src/main/java/fr/unilim/fr/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/fr/spaceinvaders/SpaceInvaders.java
@@ -63,4 +63,13 @@ public class SpaceInvaders {
 		return ((x >= 0) && (x < longueur)) && ((y >= 0) && (y < hauteur));
 	}
 
+	public void deplacerVaisseauVersLaDroite() {
+		 if (vaisseau.abscisse()< (longueur-1)) vaisseau.seDeplacerVersLaDroite();	
+	}
+
+	public void deplacerVaisseauVersLaGauche() {
+		if (vaisseau.abscisse()> 0) vaisseau.seDeplacerVersLaGauche();
+		
+	}
+
 }
