9fec55dbcd8d4e9d1efa96b702c18e2f5110e5f1
iut <iut@10.0.2.15>
2019-04-29 09:31:12 +0200

    vaisseau avec dimension + test position hors espace de jeu

---
+++ src/main/java/fr/unilim/fr/spaceinvaders/utils/DebordementEspaceJeuException.java
@@ -0,0 +1,9 @@
+package fr.unilim.fr.spaceinvaders.utils;
+
+@SuppressWarnings("serial")
+public class DebordementEspaceJeuException extends RuntimeException {
+
+    public DebordementEspaceJeuException(String message) {
+	    super(message);
+    }
+}
